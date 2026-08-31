b5370290338d0e64fc742cbd587f8bbab07e5b99
Pujan Upadhyay <updpujan1@gmail.com>
2026-08-31 17:07:12 +0545

    updated the file with more learned context

--- variables/variables.js
+++ variables/variables.js
@@ -1,12 +1,19 @@
-//let variable: works in the block scope
+//-----variables------------------
+//variables are containers for storing data values
+//here in js varialbs dont have fixed types as it is dynamically typed language
+
+
+//let variable: works in the block scope and cannot redeclard inside same scope but can be reassigned
 let name = "John Doe";
 console.log(name);
 
-//var variable: works everywhere even when decelared within a scope
+//var variable: works everywhere even when decelared within a scope {except fucntion scope deceleration} and cannot be redeclared but can be reassigned
 var age = 30;
 console.log(age);
 
 //const variable
+//cost varaible must be assigned a value when declared else throws error
+//var also allows redeclaration 
 const COUNTRY = "Nepal";
 console.log(COUNTRY);
 
@@ -20,4 +27,14 @@ console.log(name);
 //console.log(name);
 
 //variable names must start with a letter, underscore or dollar sign and can be combinations of letters, numbers, underscores, and dollar signs.
+//variables value can be reassigned to different data types too
+//js is case sensitive language so pujan and PUJAN are different variables
+//if variable is printed without being assigned a value it will return undefined (except const, it throw error)
+
+/*
+function variableTypes() {
+    var variable1 = "Hello";
+}
+console.log(variable1); //throws error because variable1 is not defined in this scope
+*/
 
