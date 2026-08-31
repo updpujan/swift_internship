44afeb3301456e31ae37db0b9e107264429290d3
Pujan Upadhyay <updpujan1@gmail.com>
2026-08-31 13:16:22 +0545

    Learned varables of js

---
+++ variables/variables.js
@@ -0,0 +1,23 @@
+//let variable: works in the block scope
+let name = "John Doe";
+console.log(name);
+
+//var variable: works everywhere even when decelared within a scope
+var age = 30;
+console.log(age);
+
+//const variable
+const COUNTRY = "Nepal";
+console.log(COUNTRY);
+
+//const error
+//COUNTRY = "USA"; //throws error because const variable cannot be reassigned but work for other variables
+name = 'Pujan Upadhyay';
+console.log(name);
+
+//reassign the variable name throws error no matter the variable types (let, var, const)
+//var name = "Pujan";
+//console.log(name);
+
+//variable names must start with a letter, underscore or dollar sign and can be combinations of letters, numbers, underscores, and dollar signs.
+
